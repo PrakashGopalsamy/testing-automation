364b9d023245e1488822f83f460f265b9eaab7de
PrakashGopalsamy <prakash91srec@gmail.com>
2022-07-18 16:33:33 +0530

    Moved common dependencies to  new project

--- src/main/java/com/infosys/test/automation/dto/CondConfig.java
+++
@@ -1,8 +0,0 @@
-package com.infosys.test.automation.dto;
-
-import org.json.simple.parser.ParseException;
-
-public interface CondConfig {
-
-    public boolean evaluateCondition(String parentRecord, String childRecord) throws ParseException;
-}
