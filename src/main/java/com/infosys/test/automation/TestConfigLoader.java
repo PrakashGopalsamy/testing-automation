8f6440ffb0842209f978b1d553a2c7e6c6801049
PrakashGopalsamy <prakash91srec@gmail.com>
2022-07-25 18:26:49 +0530

    Created custom exceptions to enforce xml structure

--- src/main/java/com/infosys/test/automation/TestConfigLoader.java
+++ src/main/java/com/infosys/test/automation/TestConfigLoader.java
@@ -2,9 +2,11 @@ package com.infosys.test.automation;
 
 import com.infosys.test.automation.constants.TestConfigConstants;
 import com.infosys.test.automation.dto.*;
+import com.infosys.test.automation.exceptions.IllegalNodeException;
 
 import javax.xml.stream.XMLInputFactory;
 import javax.xml.stream.XMLStreamConstants;
+import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamReader;
 import java.io.*;
 import java.util.Locale;
@@ -21,17 +23,18 @@ public class TestConfigLoader {
         this.testConfigFile = testConfigFile;
     }
 
-    public TestConfig load() throws Exception{
+    public TestConfig load() throws IllegalNodeException, FileNotFoundException, XMLStreamException {
         FileInputStream fileInputStream = new FileInputStream(testConfigFile);
         InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream);
         XMLInputFactory xmlInputFactory = XMLInputFactory.newInstance();
         xmlInputFactory.setProperty("javax.xml.stream.isCoalescing", true);
         XMLStreamReader xmlStreamReader = xmlInputFactory.createXMLStreamReader(inputStreamReader);
+        String previousElement = null;
         while(xmlStreamReader.hasNext()){
             int eventType = xmlStreamReader.next();
             switch (eventType){
                 case XMLStreamConstants.START_ELEMENT:{
-                    processStartElement(xmlStreamReader);
+                    previousElement = processStartElement(xmlStreamReader,previousElement);
                     break;
                 }
                 case XMLStreamConstants.END_ELEMENT:{
@@ -44,7 +47,7 @@ public class TestConfigLoader {
 
     }
 
-    private void processStartElement(XMLStreamReader xmlStreamReader) throws Exception{
+    private String processStartElement(XMLStreamReader xmlStreamReader,String previousElement) throws IllegalNodeException, XMLStreamException {
         String name = xmlStreamReader.getLocalName();
 //        System.out.println("Element Name : "+name);
         switch (name.toUpperCase(Locale.ROOT)){
@@ -55,34 +58,41 @@ public class TestConfigLoader {
                 break;
             }
             case TestConfigConstants.SOURCE:{
-                if (currentBuilder !=null){
-                    builderTracker.push(currentBuilder);
+                if (currentBuilder == null || !(currentBuilder instanceof TestConfig.TestConfigBuilder || currentBuilder instanceof SourceConfig.SourceConfigBuilder)){
+                    throw new IllegalNodeException("Element \""+name+"\" is present inside the element \""+previousElement+"\" instead of the container elements : "+TestConfigConstants.TEST+"|"+TestConfigConstants.SOURCE);
                 }
+                builderTracker.push(currentBuilder);
                 currentBuilder = new SourceConfig.SourceConfigBuilder();
                 ((SourceConfig.SourceConfigBuilder) currentBuilder).setType(xmlStreamReader.getAttributeValue(0));
                 break;
             }
             case TestConfigConstants.TARGET:{
-                if (currentBuilder !=null){
-                    builderTracker.push(currentBuilder);
+                if (currentBuilder == null || !(currentBuilder instanceof TestConfig.TestConfigBuilder)){
+                    throw new IllegalNodeException("Element \""+name+"\" is present inside the element \""+previousElement+"\" instead of the container element : "+TestConfigConstants.TEST);
                 }
+                builderTracker.push(currentBuilder);
                 currentBuilder = new TargetConfig.TargetConfigBuilder();
                 ((TargetConfig.TargetConfigBuilder) currentBuilder).setType(xmlStreamReader.getAttributeValue(0));
                 break;
             }
             case TestConfigConstants.TESTCASE:{
-                if (currentBuilder !=null){
-                    builderTracker.push(currentBuilder);
+                if (currentBuilder == null || !(currentBuilder instanceof TestConfig.TestConfigBuilder)){
+                    throw new IllegalNodeException("Element \""+name+"\" is present inside the element \""+previousElement+"\" instead of the container element : "+TestConfigConstants.TEST);
                 }
+                builderTracker.push(currentBuilder);
                 currentBuilder = new TestCaseConfig.TestCaseConfigBuilder();
                 ((TestCaseConfig.TestCaseConfigBuilder) currentBuilder).setType(xmlStreamReader.getAttributeValue(0));
                 break;
             }
             case TestConfigConstants.MULTIJOINCONDITION:
             case TestConfigConstants.MULTIFILTERCONDITION :{
-                if (currentBuilder !=null){
-                    builderTracker.push(currentBuilder);
+                if (name.equalsIgnoreCase(TestConfigConstants.MULTIJOINCONDITION) && (currentBuilder == null || !(currentBuilder instanceof MultiCondConfig.MultiCondConfigBuilder || currentBuilder instanceof SourceConfig.SourceConfigBuilder || currentBuilder instanceof TargetConfig.TargetConfigBuilder))){
+                    throw new IllegalNodeException("Element \""+name+"\" is present inside the element \""+previousElement+"\" instead of the container elements : "+TestConfigConstants.MULTIJOINCONDITION+" | "+TestConfigConstants.SOURCE+" | "+TestConfigConstants.TARGET);
                 }
+                if (name.equalsIgnoreCase(TestConfigConstants.MULTIFILTERCONDITION) && (currentBuilder == null || !(currentBuilder instanceof MultiCondConfig.MultiCondConfigBuilder || currentBuilder instanceof SourceConfig.SourceConfigBuilder))){
+                    throw new IllegalNodeException("Element \""+name+"\" is present inside the element \""+previousElement+"\" instead of the container elements : "+TestConfigConstants.MULTIFILTERCONDITION+" | "+TestConfigConstants.SOURCE);
+                }
+                builderTracker.push(currentBuilder);
                 currentBuilder = new MultiCondConfig.MultiCondConfigBuilder();
                 int attriCount = xmlStreamReader.getAttributeCount();
 //                System.out.println("Multi Condition elem");
@@ -95,14 +105,21 @@ public class TestConfigLoader {
             }
             case TestConfigConstants.SINGLEJOINCONDITION:
             case TestConfigConstants.SINGLEFILTERCONDITION:{
-                if (currentBuilder !=null){
-                    builderTracker.push(currentBuilder);
+                if (name.equalsIgnoreCase(TestConfigConstants.SINGLEJOINCONDITION) && (currentBuilder == null || !(currentBuilder instanceof MultiCondConfig.MultiCondConfigBuilder || currentBuilder instanceof SourceConfig.SourceConfigBuilder || currentBuilder instanceof TargetConfig.TargetConfigBuilder))){
+                    throw new IllegalNodeException("Element \""+name+"\" is present inside the element \""+previousElement+"\" instead of the container elements : "+TestConfigConstants.MULTIJOINCONDITION+" | "+TestConfigConstants.SOURCE+" | "+TestConfigConstants.TARGET);
                 }
+                if (name.equalsIgnoreCase(TestConfigConstants.SINGLEFILTERCONDITION) && (currentBuilder == null || !(currentBuilder instanceof MultiCondConfig.MultiCondConfigBuilder || currentBuilder instanceof SourceConfig.SourceConfigBuilder))){
+                    throw new IllegalNodeException("Element \""+name+"\" is present inside the element \""+previousElement+"\" instead of the container elements : "+TestConfigConstants.MULTIFILTERCONDITION+" | "+TestConfigConstants.SOURCE);
+                }
+                builderTracker.push(currentBuilder);
                 currentBuilder = new SingleCondConfig.SingleCondConfigBuilder();
                 ((SingleCondConfig.SingleCondConfigBuilder) currentBuilder).setOperator(xmlStreamReader.getAttributeValue(0));
                 break;
             }
             case TestConfigConstants.COLUMN:{
+                if (currentBuilder == null || !(currentBuilder instanceof SingleCondConfig.SingleCondConfigBuilder)){
+                    throw new IllegalNodeException("Element \""+name+"\" is present inside the element \""+previousElement+"\" instead of the container elements : "+TestConfigConstants.SINGLEFILTERCONDITION+" | "+TestConfigConstants.SINGLEJOINCONDITION);
+                }
                 int event = xmlStreamReader.next();
                 if (event == XMLStreamConstants.CHARACTERS && !xmlStreamReader.isWhiteSpace()){
                     String column = xmlStreamReader.getText().trim();
@@ -111,6 +128,9 @@ public class TestConfigLoader {
                 break;
             }
             case TestConfigConstants.VALUE:{
+                if (currentBuilder == null || !(currentBuilder instanceof SingleCondConfig.SingleCondConfigBuilder)){
+                    throw new IllegalNodeException("Element \""+name+"\" is present inside the element \""+previousElement+"\" instead of the container elements : "+TestConfigConstants.SINGLEFILTERCONDITION+" | "+TestConfigConstants.SINGLEJOINCONDITION);
+                }
                 int event = xmlStreamReader.next();
                 if (event == XMLStreamConstants.CHARACTERS && !xmlStreamReader.isWhiteSpace()){
                     String value = xmlStreamReader.getText().trim();
@@ -122,22 +142,22 @@ public class TestConfigLoader {
                 xmlStreamReader.next();
                 if (currentBuilder instanceof TestConfig.TestConfigBuilder){
                     ((TestConfig.TestConfigBuilder) currentBuilder).setName(xmlStreamReader.getText().trim());
-                }
-                if (currentBuilder instanceof SourceConfig.SourceConfigBuilder){
+                }else if (currentBuilder instanceof SourceConfig.SourceConfigBuilder){
                     ((SourceConfig.SourceConfigBuilder) currentBuilder).setName(xmlStreamReader.getText().trim());
-                }
-                if (currentBuilder instanceof TargetConfig.TargetConfigBuilder){
+                }else if (currentBuilder instanceof TargetConfig.TargetConfigBuilder){
                     ((TargetConfig.TargetConfigBuilder) currentBuilder).setName(xmlStreamReader.getText().trim());
-                }
-                if (currentBuilder instanceof TestCaseConfig.TestCaseConfigBuilder){
+                }else if (currentBuilder instanceof TestCaseConfig.TestCaseConfigBuilder){
                     ((TestCaseConfig.TestCaseConfigBuilder) currentBuilder).setName(xmlStreamReader.getText().trim());
+                }else{
+                    throw new IllegalNodeException("Element \""+name+"\" is present inside the element \""+previousElement+"\" instead of the container elements : "+TestConfigConstants.TEST+" | "+TestConfigConstants.SOURCE+" | "+TestConfigConstants.TARGET+" | "+TestConfigConstants.TESTCASE);
                 }
                 break;
             }
             default:{
-                processProperties(xmlStreamReader,name);
+                processProperties(xmlStreamReader,name,previousElement);
             }
         }
+        return name;
     }
 
     private void processEndElement(XMLStreamReader xmlStreamReader){
@@ -224,30 +244,27 @@ public class TestConfigLoader {
         }
     }
 
-    private void processProperties(XMLStreamReader xmlStreamReader,String elementName) throws Exception{
-        if (!(elementName.equalsIgnoreCase("Sources") &&
-                elementName.equalsIgnoreCase("MultiFilterCondition") &&
-                elementName.equalsIgnoreCase("SingleFilterCondition") &&
-                elementName.equalsIgnoreCase("TestCases") &&
-                elementName.equalsIgnoreCase("SingleFilterCondition") &&
-                elementName.equalsIgnoreCase("SingleJoinCondition") &&
-                elementName.equalsIgnoreCase("column") &&
-        elementName.equalsIgnoreCase("value"))
+    private void processProperties(XMLStreamReader xmlStreamReader,String elementName,String parentElement) throws IllegalNodeException, XMLStreamException {
+        if (!(elementName.equalsIgnoreCase(TestConfigConstants.SOURCES) &&
+                elementName.equalsIgnoreCase(TestConfigConstants.TESTCASE))
         ){
             int event = xmlStreamReader.next();
             if (event == XMLStreamConstants.CHARACTERS && !xmlStreamReader.isWhiteSpace()){
                 String value = xmlStreamReader.getText().trim();
+                String exceptionMessage = "The element \""+elementName+"\" is present inside the element \""+parentElement+"\" instead of the container elements : "+TestConfigConstants.TEST+" | "+TestConfigConstants.SOURCE+" | "+TestConfigConstants.TARGET+" | "+TestConfigConstants.TESTCASE;
+                if (currentBuilder == null){
+                    throw new IllegalNodeException(exceptionMessage);
+                }
                 if (currentBuilder instanceof TestConfig.TestConfigBuilder){
                     ((TestConfig.TestConfigBuilder) currentBuilder).addProperty(elementName.toLowerCase(Locale.ROOT),value);
-                }
-                if (currentBuilder instanceof SourceConfig.SourceConfigBuilder){
+                } else if (currentBuilder instanceof SourceConfig.SourceConfigBuilder){
                     ((SourceConfig.SourceConfigBuilder) currentBuilder).addProperty(elementName.toLowerCase(Locale.ROOT),value);
-                }
-                if (currentBuilder instanceof TargetConfig.TargetConfigBuilder){
+                } else if (currentBuilder instanceof TargetConfig.TargetConfigBuilder){
                     ((TargetConfig.TargetConfigBuilder) currentBuilder).addProperty(elementName.toLowerCase(Locale.ROOT),value);
-                }
-                if (currentBuilder instanceof TestCaseConfig.TestCaseConfigBuilder){
+                }else if (currentBuilder instanceof TestCaseConfig.TestCaseConfigBuilder){
                     ((TestCaseConfig.TestCaseConfigBuilder) currentBuilder).addProperty(elementName.toLowerCase(Locale.ROOT),value);
+                } else{
+                    throw new IllegalNodeException(exceptionMessage);
                 }
             }
 
---
+++ src/main/java/com/infosys/test/automation/exceptions/IllegalNodeException.java
@@ -0,0 +1,7 @@
+package com.infosys.test.automation.exceptions;
+
+public class IllegalNodeException extends Exception{
+    public IllegalNodeException(String exceptionMessage){
+        super(exceptionMessage);
+    }
+}
